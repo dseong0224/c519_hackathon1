480d0342b34b9d6c7d5b9346ec3e5e8d59f9c012
Steve Min <stevekmin@gmail.com>
2019-06-06 16:57:41 -0700

    add player two update functionality for player attack update functionality for player health fix cosmetics

--- 3_king_of_tokyo/assets/js/game.js
+++ 3_king_of_tokyo/assets/js/game.js
@@ -4,27 +4,45 @@ class Game {
         this.getHealth = this.getHealth.bind(this);
 
         this.roll = this.roll.bind(this);
+        $('#rollButton').on('click', this.roll);
 
         // this.player1 = new Player(this.roll);
-        this.players = [new Player(this.roll)];
+        this.players = [new Player("Player1",this.roll), new Player("Player2",this.roll)];
         this.currentPlayer = 0;
+        this.otherPlayers = !0
         this.diceRoll = new Dice(); //rolls a dice
     }
 
     roll() {
+        
+        if (this.currentPlayer === this.players.length) {
+            this.currentPlayer = 0;
+        }
         this.diceValue1 = this.diceRoll.roll();
-        console.log('Dice roll result:', this.diceValue1);
+        console.log('Dice roll result:', this.diceValue1);//Player 2
 
         // this.diceValue1 = this.diceRoll.roll();
 
         if(typeof this.diceValue1 === 'number'){
             this.getPoints();
         }
+        else if(this.diceValue1 === "claw"){
+            this.players[1].removeHealth(1)
+        }
+        
         else {
             this.getHealth()
         }
-
+        this.currentPlayer++;
     }
+
+    // attack(player) {
+    //     // this.player
+    //     if(this.diceValue1 === 'claw') {
+    //         this.players[1].removeHealth(1);
+    //         // this.players[this.currentPlayer].removeHealth(1)
+    //     }
+    // }
 
     getHealth () {
         // console.log('current die:',this.diceValue1);
@@ -32,28 +50,30 @@ class Game {
         // if(this.diceValue1 === 'heart') {
         //     this.player1.addHealth(1);
         // }
-        if(this.diceValue1 === 'health') {
-            this.players[0].addHealth(1);
-            this.players[this.currentPlayer].addHealth(1)
+        if(this.diceValue1 === 'heart') {
+            this.players[this.currentPlayer].addHealth(1);
+        } else if(this.diceValue1 === 'claw') {
+            this.players[this.otherPlayers].removeHealth(1);  
         }
+        console.log("Player:", this.players[this.currentPlayer])
 
-        console.log('current health', this.players[0].accumulatedHealth())
-        $('.health').append(this.players[0].accumulatedHealth());
+        console.log('current health', this.players[this.currentPlayer].accumulatedHealth())
+        $('.health').append(this.players[this.currentPlayer].accumulatedHealth());
     }
 
     getPoints () {
         // console.log('current die:',this.diceValue1);
 
         // this.roll();
-            this.players[0].addPoint(this.diceValue1);
-            console.log("current points", this.players[0].accumulatedPoints());
-            $('.points').append(this.players[0].accumulatedPoints());
+            this.players[this.currentPlayer].addPoint(this.diceValue1);
+            console.log("current points", this.players[this.currentPlayer].accumulatedPoints());
+            $('.points').append(this.players[this.currentPlayer].accumulatedPoints());
 
-        if(this.players[0].accumulatedPoints() >= 20){
+        if(this.players[this.currentPlayer].accumulatedPoints() >= 20){
             this.winCondition()
         }
     }
-
+    
     loseCondition (){
         // heart = 0
         alert ('You lost')
@@ -61,7 +81,7 @@ class Game {
 
 
     winCondition(){
-        alert ('You won!')
+        alert (this.players[this.currentPlayer].name + ' won!')
     }
 
 }

3d8e864fa1bae5ce8e1ae8a3d4e505fe2763b668
Daniel Seong <dseong0224@gmail.com>
2019-06-06 07:47:58 -0700

    revised css and added health&points(semi-functional)

--- 3_king_of_tokyo/assets/js/dice.js
+++ 3_king_of_tokyo/assets/js/dice.js
@@ -1,23 +1,12 @@
 class Dice {
     constructor(){
-        this.outcomeArray = [1,2,3,"claw","heart"]; //set array for possible player outcomes
+        this.outcomeArray = [1,2,3,'heart']; //set array for possible player outcomes
         this.pickedDice = [];
-
     }
     roll(){
         //create random number generator to pick from array
         var shuffledArray = this.outcomeArray.sort(() => Math.random() - 0.5);
-
-        // this.pickedDice.push(shuffledArray[0]);
-
-        console.log('Outcome:', this.pickedDice);
-
-        console.log('array:',shuffledArray[0]);
-
-
-        return shuffledArray;
-    }
-    pickDice(){
-        this.roll()
+        this.pickedDice.push(shuffledArray[0]);
+        return this.pickedDice;
     }
 }
--- 3_king_of_tokyo/assets/js/game.js
+++ 3_king_of_tokyo/assets/js/game.js
@@ -1,14 +1,13 @@
 class Game {
     constructor () {
-        this.getDiceInfo = this.getDiceInfo.bind(this);
+        this.getPoints = this.getPoints.bind(this);
+        this.getHealth = this.getHealth.bind(this);
 
-        this.player1 = new Player(this.getDiceInfo);
-        this.player2 = new Player(this.getDiceInfo);
-        this.diceRoll = new Dice();
+        this.player1 = new Player(this.getPoints,this.getHealth); // makes player
 
+        this.diceRoll = new Dice(); //rolls a dice
 
-        this.diceValue1 = null;
-        this.diceValue2 = null;
+        // this.diceValue1 = null;
         this.roll = this.roll.bind(this);
 
     }
@@ -18,13 +17,45 @@ class Game {
         console.log('Dice roll result:', diceRollResult);
 
         this.diceValue1 = diceRollResult[0];
-        this.diceValue2 = diceRollResult[1];
 
-        this.diceRoll.pickedDice = [];
+        if(typeof this.diceValue1 === 'number'){
+            this.getPoints();
+        } else {
+            this.getHealth()
+        }
+
     }
 
-    getDiceInfo(){
-        console.log(this.diceValue1, this.diceValue2)
+    getHealth () {
+        console.log('current die:',this.diceValue1);
+
+        if(this.diceValue1 === 'heart') {
+            this.player1.addHealth(1);
+        }
+
+        console.log('current health', this.player1.accumulatedHealth())
+
+    }
+
+    getPoints () {
+        console.log('current die:',this.diceValue1);
+
+
+            this.player1.addPoint(this.diceValue1);
+            console.log("current points", this.player1.accumulatedPoints());
+
+        if(this.player1.accumulatedPoints() >= 20){
+            this.winCondition()
+        }
+    }
+
+    loseCondition (){
+        alert ('you lost')
+    }
+
+
+    winCondition(){
+        alert ('You won!')
     }
 
 

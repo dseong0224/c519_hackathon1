a516a8ab96afe92bcf8abc4ef05e88032eafda25
David Rabosky <davidrabosky@gmail.com>
2019-06-10 19:03:21 -0700

    MVP again Done

--- 3_king_of_tokyo/assets/js/game.js
+++ 3_king_of_tokyo/assets/js/game.js
@@ -8,6 +8,11 @@ class Game {
 
         this.allPlayers = [new Player("player1"), new Player("player2"), new Player("player3"), new Player("player4")];
         this.currentPlayer = 0;
+        this.otherPlayers = this.allPlayers.slice(this.currentPlayer,this.currentPlayer+1);
+        //check if the player is current player by looping through all player array index
+            //add all those other palyers to a temp array
+            //damage all players in the new array
+            //use slice and splice
         this.playerOne = 0;
         this.playerTwo = 1;
         this.playerThree = 2;
@@ -16,6 +21,13 @@ class Game {
     }
 
     rollDice() {
+        if(this.allPlayers[this.currentPlayer].health <= 0){
+            this.currentPlayer = this.currentPlayer + 1 % 4;
+            this.rollDice();
+            return;
+        }
+
+
 
         this.rollResult = this.newDice.roll();
         
@@ -23,7 +35,6 @@ class Game {
             this.updateVP();
         }
         else if(this.rollResult === "claw"){
-            // this.allPlayers[this.playerTwo].takeDamage(1)
             this.updateHP()
         }
         
@@ -31,7 +42,7 @@ class Game {
             this.updateHP()
         }
         this.displayAction(this.rollResult);
-
+        this.otherPlayers = this.allPlayers.slice(this.currentPlayer,this.currentPlayer+1);
         this.currentPlayer++;
 
         if(this.currentPlayer === this.allPlayers.length){
@@ -52,7 +63,6 @@ class Game {
         // if (this.playerFour === this.allPlayers.length) {
         //     this.playerFour = 0;
         // }
-
     }
 
     displayAction (action){
@@ -87,28 +97,34 @@ class Game {
                 return false
             }
         } else if(this.rollResult === 'claw') {
-            if(this.currentPlayer === 0){
-                this.allPlayers[this.playerTwo].takeDamage(1);
-                this.allPlayers[this.playerThree].takeDamage(1);
-                this.allPlayers[this.playerFour].takeDamage(1);
-            } else if(this.currentPlayer === 1){
-                this.allPlayers[this.playerOne].takeDamage(1);
-                this.allPlayers[this.playerThree].takeDamage(1);
-                this.allPlayers[this.playerFour].takeDamage(1);
-            } else if(this.currentPlayer === 2){
-                this.allPlayers[this.playerOne].takeDamage(1);
-                this.allPlayers[this.playerTwo].takeDamage(1);
-                this.allPlayers[this.playerFour].takeDamage(1);
-            } else if(this.currentPlayer === 3){
-                this.allPlayers[this.playerOne].takeDamage(1);
-                this.allPlayers[this.playerTwo].takeDamage(1);
-                this.allPlayers[this.playerThree].takeDamage(1);
+
+            for(var i=0; i<this.otherPlayers.length; i++){
+                this.otherPlayers[i].takeDamage(1);
             }
 
-            // this.allPlayers[this.playerTwo].takeDamage(1);
-            // this.allPlayers[this.playerThree].takeDamage(1);
-            // this.allPlayers[this.playerFour].takeDamage(1);
+
+            // if(this.currentPlayer === 0){
+            //     this.allPlayers[this.playerTwo].takeDamage(1);
+            //     this.allPlayers[this.playerThree].takeDamage(1);
+            //     this.allPlayers[this.playerFour].takeDamage(1);
+            // } else if(this.currentPlayer === 1){
+            //     this.allPlayers[this.playerOne].takeDamage(1);
+            //     this.allPlayers[this.playerThree].takeDamage(1);
+            //     this.allPlayers[this.playerFour].takeDamage(1);
+            // } else if(this.currentPlayer === 2){
+            //     this.allPlayers[this.playerOne].takeDamage(1);
+            //     this.allPlayers[this.playerTwo].takeDamage(1);
+            //     this.allPlayers[this.playerFour].takeDamage(1);
+            // } else if(this.currentPlayer === 3){
+            //     this.allPlayers[this.playerOne].takeDamage(1);
+            //     this.allPlayers[this.playerTwo].takeDamage(1);
+            //     this.allPlayers[this.playerThree].takeDamage(1);
+            // }
+
+
         }
+
+
 
         $('.healthOne').text("Health Points: " + this.allPlayers[this.playerOne].totalHP());
         $('.healthTwo').text("Health Points: " + this.allPlayers[this.playerTwo].totalHP());
@@ -119,16 +135,29 @@ class Game {
         $('.pointsThree').text("Victory Points: " + this.allPlayers[this.playerThree].totalVP());
         $('.pointsFour').text("Victory Points: " + this.allPlayers[this.playerFour].totalVP());
 
-        for( var playerIndex in this.allPlayers){
-            if(this.allPlayers[playerIndex].totalHP() <= 0){
-                this.loseCondition(this.allPlayers[playerIndex].name)
-            }
-        }
+        // for( var playerIndex in this.allPlayers){
+        //     if(this.allPlayers[playerIndex].totalHP() <= 0){
+        //         this.allPlayers.splice(playerIndex, 1)
+        //     }
+        // }
 
+        if(this.allPlayers[this.playerOne].totalHP() <= 0){
+            $("#playerContainer1").addClass("hidden");
+            console.log("player1 is dead");
+        } if(this.allPlayers[this.playerTwo].totalHP() <= 0){
+            $("#playerContainer2").addClass("hidden");
+            console.log("player2 is dead")
+        } if(this.allPlayers[this.playerThree].totalHP() <= 0){
+            $("#playerContainer3").addClass("hidden");
+            console.log("player3 is dead")
+        } if(this.allPlayers[this.playerFour].totalHP() <= 0){
+            $("#playerContainer4").addClass("hidden");
+            console.log("player4 is dead")
+        }
     }
 
     updateVP () {
-            this.allPlayers[this.currentPlayer].gainVP(this.rollResult);
+        this.allPlayers[this.currentPlayer].gainVP(this.rollResult);
 
 
         $('.healthOne').text("Health Points: " + this.allPlayers[this.playerOne].totalHP());
